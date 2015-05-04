4b26692e8075ca71f55b6cd08e24a3279e89da7c
Efosa Osamwonyi <efosa.osamwonyi346@gmail.com>
2015-05-04 14:19:22 -0400

    fixed

---
+++ Public/app.js
@@ -0,0 +1,26 @@
+$( "#buttonView3" ).click(function() {
+ console.log('clickity');
+  $( "#work" ).empty();
+});
+
+// $.ajax({
+//   method: "GET",
+//   url: "/"
+// }).done(function(debts) {
+//   // .map is equivalent to the code below... it is a shortcut
+
+//   // var debtEls = debts.map(function(debt) {
+//   //   return Mustache.render(template, debt);
+//   // })
+
+//   var debtEls = [];
+
+//   debts.forEach(function(debt) {
+//     var html = Mustache.render(template, debt);
+//     debtEls.push(html);
+//   });
+
+//   //
+
+//   $('tbody').append(debtEls);
+// });
